8e6b5592d3907ba18b50b16c1d853e7ee8f6cc35
rthandi <rthandi1@sheffield.ac.uk>
2020-03-24 17:57:48 +0000

    Robbie: Created basic mock-ups for navbar and posts.

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var $ = require('jquery');
+// var bootstrap = require('bootstrap');
 
+var logger = require('morgan');
 var indexRouter = require('./routes/index');
+
 var usersRouter = require('./routes/users');
 
 var app = express();
