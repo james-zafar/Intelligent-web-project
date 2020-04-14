57a915ec9b86d6a1bb69363a5a58bad94a93144c
rthandi <rthandi1@sheffield.ac.uk>
2020-04-14 22:18:51 +0100

    updated dependencies.

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var $ = require('jquery');
-// var bootstrap = require('bootstrap');
 
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
