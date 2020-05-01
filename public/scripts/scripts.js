410ee703a5278f1b10ca6b37e4c2d7e4ec9a77f2
rthandi <rthandi1@sheffield.ac.uk>
2020-05-01 15:57:25 +0100

    added offline messages

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -17,3 +17,43 @@ function loadData() {
   //Believe this should get stuff from database.js when we have stuff to store
   return null;
 }
+
+/**
+ * When the client gets off-line, it shows an off line warning to the user
+ * so that it is clear that the data is stale
+ */
+window.addEventListener('offline', function(e) {
+  // Queue up events for server.
+  console.log("You are offline");
+  showOfflineWarning();
+}, false);
+
+/**
+ * When the client gets online, it hides the off line warning
+ */
+window.addEventListener('online', function(e) {
+  // Resync data with server.
+  console.log("You are online");
+  hideOfflineWarning();
+  loadData();
+}, false);
+
+
+function showOfflineWarning(){
+  if (document.getElementById('offline_div')!=null)
+    document.getElementById('offline_div').style.display='block';
+}
+
+function hideOfflineWarning(){
+  if (document.getElementById('offline_div')!=null)
+    document.getElementById('offline_div').style.display='none';
+}
+
+
+/**
+ * it shows the city list in the browser
+ */
+function showCityList() {
+  if (document.getElementById('city_list')!=null)
+    document.getElementById('city_list').style.display = 'block';
+}
