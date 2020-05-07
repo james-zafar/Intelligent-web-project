0b84711e91797c380295ae2b00486eac2c84770d
Samuel Spiers <sammyspiers@yahoo.co.uk>
2020-05-07 14:15:13 +0100

    Started to create indexedDB

--- public/scripts/database.js
+++ public/scripts/database.js
@@ -1,1 +1,81 @@
-//This file should store the data received into local storage
+let dbPromise;
+
+const DB_NAME = 'db_my_story_1';
+const STORE_NAME = 'store_stories';
+
+/**
+ * Initialises the database
+ */
+function initDatabase() {
+    dbPromise = idb.openDb(DB_NAME, 1, function (upgradeDb) {
+        if (!upgradeDb.objectStoreNames.contains(STORE_NAME)) {
+            let forecastDB = upgradeDb.createObjectStore(STORE_NAME, {keyPath: 'id', autoIncrement: true});
+            forecastDB.createIndex('text', 'text', {unique: false, multiEntry: true});
+            forecastDB.createIndex('image', 'image', {unique: false, multiEntry: true});
+            forecastDB.createIndex('date', 'date', {unique: false, multiEntry: true});
+            forecastDB.createIndex('user_id', 'user_id', {unique: false, multiEntry: true});
+        }
+    });
+}
+
+/**
+ * Saves stories for a user in indexedDB
+ * @param user
+ * @param storyObject
+ */
+function storeCachedData(user, storyObject) {
+    console.log('inserting: '+ JSON.stringify(storyObject));
+    if (dbPromise) {
+        dbPromise.then(async db => {
+            let tx = db.transaction(STORE_NAME, 'readwrite');
+            let store = tx.objectStore(STORE_NAME);
+            await store.put(storyObject);
+            return tx.complete;
+        }).then(function () {
+            console.log('added item to the store! '+ JSON.stringify(storyObject));
+        }).catch(function (e) {
+            localStorage.setItem(user, JSON.stringify(storyObject));
+        });
+    } else  {
+        localStorage.setItem(user, JSON.stringify(storyObject));
+    }
+}
+
+/**
+ * Gets story data for a user from the database
+ * @param user
+ * @param date
+ * @returns {*}
+ */
+function getCachedData(user, date) {
+    if (dbPromise) {
+        dbPromise.then(function (db) {
+            console.log('fetching: '+ user);
+            let tx = db.transaction(STORE_NAME, 'readonly');
+            let store = tx.objectStore(STORE_NAME);
+            let index = store.index('location');
+            return index.getAll(IDBKeyRange.only(user));
+        }).then(function (readingsList) {
+            if (readingsList && readingsList.length>0){
+                let max;
+                for (let elem of readingsList)
+                    if (!max || elem.date > max.date)
+                        max= elem;
+                if (max) addToResults(max);
+            } else {
+                const value = localStorage.getItem(user);
+                if (value == null) {
+                    addToResults({city: city, date: date});
+                }
+                else {
+                    addToResults(value);
+                }
+            }
+        });
+    } else {
+        const value = localStorage.getItem(user);
+        if (value == null)
+            addToResults( {user: user, date: date});
+        else addToResults(value);
+    }
+}
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,17 +1,59 @@
 function initPostsApp() {
-  // loadData();
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('./service-worker.js')
-      .then(function(registration) {
-        //success
-        console.log('Service Worker Registered with scope: ' + registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-  }
+    // loadData();
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function(registration) {
+            //success
+            console.log('Service Worker Registered with scope: ' + registration.scope);
+            }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+            });
+    }
+    if ('indexedDB' in window) {
+        initDatabase();
+    } else {
+        console.log('This browser doesn\'t support IndexedDB');
+    }
 }
+
+/**
+ * Loads stories for a specific user
+ * @param user
+ * @param date
+ */
+function loadStorydata(user, date) {
+    const input = JSON.stringify({user: user, date: date});
+    $.ajax({
+        url: '/stories',
+        data: input,
+        contentType: 'application/json',
+        type: 'POST',
+        success: function (dataR) {
+            // no need to JSON parse the result, as we are using
+            // dataType:json, so JQuery knows it and unpacks the
+            // object for us before returning it
+            // addToResults(dataR);
+            storeCachedData(dataR.user, dataR);
+            // if (document.getElementById('offline_div')!=null)
+            //     document.getElementById('offline_div').style.display='none';
+        },
+        // the request to the server has failed. Let's show the cached data
+        error: function (xhr, status, error) {
+            // showOfflineWarning();
+            getCachedData(user, date);
+            // const dvv = document.getElementById('offline_div');
+            // if (dvv != null) {
+            //     dvv.style.display='block';
+            // }
+        }
+    });
+    // // hide the list of cities if currently shown
+    // if (document.getElementById('city_list') != null)
+    //     document.getElementById('city_list').style.display = 'none';
+}
+
 function sendAjaxQuery(url, data) {
     $.ajax({
         url: url ,
@@ -45,8 +87,8 @@ function send(url) {
 }
 
 function loadData() {
-  //Believe this should get stuff from database.js when we have stuff to store
-  return null;
+    //Believe this should get stuff from database.js when we have stuff to store
+    return null;
 }
 
 /**
@@ -54,37 +96,39 @@ function loadData() {
  * so that it is clear that the data is stale
  */
 window.addEventListener('offline', function(e) {
-  // Queue up events for server.
-  console.log("You are offline");
-  showOfflineWarning();
+    // Queue up events for server.
+    console.log("You are offline");
+    showOfflineWarning();
 }, false);
 
 /**
  * When the client gets online, it hides the off line warning
  */
 window.addEventListener('online', function(e) {
-  // Resync data with server.
-  console.log("You are online");
-  hideOfflineWarning();
-  loadData();
+    // Resync data with server.
+    console.log("You are online");
+    hideOfflineWarning();
+    loadData();
 }, false);
 
 
-function showOfflineWarning(){
-  if (document.getElementById('offline_div')!=null)
-    document.getElementById('offline_div').style.display='block';
+function showOfflineWarning() {
+    if (document.getElementById('offline_div') != null) {
+        document.getElementById('offline_div').style.display='block';
+    }
 }
 
-function hideOfflineWarning(){
-  if (document.getElementById('offline_div')!=null)
-    document.getElementById('offline_div').style.display='none';
+function hideOfflineWarning() {
+    if (document.getElementById('offline_div') != null) {
+        document.getElementById('offline_div').style.display='none';
+    }
 }
 
 
-/**
- * it shows the city list in the browser
- */
-function showCityList() {
-  if (document.getElementById('city_list')!=null)
-    document.getElementById('city_list').style.display = 'block';
-}
+// /**
+//  * it shows the city list in the browser
+//  */
+// function showCityList() {
+//     if (document.getElementById('city_list')!=null)
+//         document.getElementById('city_list').style.display = 'block';
+// }
