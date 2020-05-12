9db245a001d8e84a95e40c083efa8609026da3aa
James Zafar <JZafar1@users.noreply.github.com>
2020-05-12 21:32:35 +0100

    Initial attempt to retrieve and display posts from DB

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+var mongodb = require('mongodb');
+
 const users = require('../controllers/users');
 const initDB = require('../controllers/init');
 initDB.init();
@@ -10,7 +12,37 @@ var Story = require('../models/stories');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    /*** The below will eventually be called via the getStories class ***/
+    var url = 'mongodb://localhost:27017/';
+    mongodb.connect(url, function (error, client) {
+        if (error) {
+            console.log("Database error: ", error);
+            res.send(error);
+        } else {
+            var db = client.db('myStory');
+            var collection = db.collection('stories');
+            /** This query needs to be amended if/when we can retrieve the username **/
+            var query = collection.find({});
+            //Use the below to check if a user is logged in
+            //if(this.user === undefined) {
+            //If username arg is provided, look up that users stories
+            //query = collection.find({'user_id': this.user});
+            //}
+            collection.find({}).toArray(function (error, results) {
+                if (error) {
+                    console.log("Error retrieving data: ", error);
+                    res.send(error);
+                } else {
+                    res.render('index', {
+                        title: 'View your timeline',
+                        //Currently uses generic profile pic
+                        profileSource: 'https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                        allStories: results
+                    });
+                }
+            });
+        }
+    });
 });
 
 router.get('/createPost', function(req, res, next) {
@@ -49,32 +81,54 @@ router.post('/createStory', function (req, res) {
     }else if(image2 === undefined) {
         images = [image0, image1];
     }
-    if(bodyText === null) {
-        res.status(403).send('No data submitted')
-    }else {
-        var theStory = new Story({
-            text: storyText,
-            images: images
-        });
-        theStory.save(function (error, response) {
-            if (error) {
-                console.log("Error ", error);
-                response.status(500).send('Internal Server Error')
-            } else {
-                res.render('createPost', {title: 'Create New Post', displayParam: ''})
-            }
-        });
-    }
+    var theStory = new Story({
+        text: storyText,
+        images: images
+    });
+    theStory.save(function (error, response) {
+        if (error) {
+            console.log("Error ", error);
+            response.status(500).send('Internal Server Error')
+        } else {
+            res.render('createPost', {title: 'Create New Post', displayParam: ''})
+        }
+    });
 });
 
+router.get('/timeline', function(req, res) {
+    //console.log("Username? ?" + req.session.userId);
+    /*** The below will eventually be called via the getStories class ***/
+    var url = 'mongodb://localhost:27017/';
+    mongodb.connect(url, function (error, client) {
+        if (error) {
+            console.log("Database error: ", error);
+            res.send(error);
+        } else {
+            var db = client.db('myStory');
+            var collection = db.collection('stories');
 
-router.get('/timeline', function(req, res, next) {
-  res.render('timeline', {
-    title: 'View your timeline',
-    profileSource: 'https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    userName: 'James Zafar',
-    timePosted: '10 Minutes Ago'
-  });
+            /** This query needs to be amended if/when we can retrieve the username **/
+            var query = collection.find({});
+            //Use the below to check if a user is logged in
+            //if(this.user === undefined) {
+                //If username arg is provided, look up that users stories
+                //query = collection.find({'user_id': this.user});
+            //}
+            collection.find({}).toArray(function (error, results) {
+                if (error) {
+                    console.log("Error retrieving data: ", error);
+                    res.send(error);
+                } else {
+                    res.render('timeline', {
+                        title: 'View your timeline',
+                        profileSource: 'https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                        allStories: results
+                    });
+                }
+            });
+        }
+    });
+
 });
 
 module.exports = router;
